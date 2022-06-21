f63fd6f99868afc7955b3e5c4a3f9ce503481bf9
y2k2mt <y2.k2mt@gmail.com>
2022-06-21 22:59:32 +0900

    Include all

--- src/parrot.cr
+++ src/parrot.cr
@@ -1,6 +1,6 @@
+require "./start_server"
+
 # TODO: Write documentation for `Parrot`
 module Parrot
   VERSION = "0.1.0"
-
-  # TODO: Put your code here
 end
---
+++ src/server.cr
@@ -0,0 +1,21 @@
+class Server
+
+  @server = HTTP::Server.new(handlers)
+
+  def self.handlers : Array(HTTP::Handler)
+    [
+      Parrot::Handler.new
+    ] of HTTP::Handler
+  end
+
+  def start : Void
+    address = @server.bind_tcp 8080
+    puts "Listening on http://#{address}"
+    @server.listen
+  end
+
+  def stop : Void
+    @server.close
+  end
+
+end
