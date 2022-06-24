a392b0dff57d826ad7de99e8f30a14873f116e74
y2k2mt <y2.k2mt@gmail.com>
2022-06-24 23:22:30 +0900

    Format

--- src/request_records.cr
+++ src/request_records.cr
@@ -1,13 +1,13 @@
 class RequestRecords
   def self.init(@@config : ServerConfig)
   end
-  
+
   def self.request_match?(context)
-    #TODO: implement
-    nil 
+    # TODO: implement
+    nil
   end
 
-  def self.recording(record : RequestRecord) 
-    #TODO: implement
+  def self.recording(record : RequestRecord)
+    # TODO: implement
   end
 end
