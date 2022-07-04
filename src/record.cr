2d582f7be4a82cdc746a0858de6fc98a39b1faad
y2k2mt <y2.k2mt@gmail.com>
2022-07-04 22:43:35 +0900

    Recording

--- src/record.cr
+++ src/record.cr
@@ -1,4 +1,16 @@
-class Record
+struct Record
+  getter uri, method, headers, body
+
+  @uri : URI
+  @method : String
+  @headers : HTTP::Headers
+  @body : IO
+
   def initialize(base_uri, request, client_response)
+    base_uri.path = request.path
+    @uri = base_uri
+    @method = request.method
+    @headers = request.headers
+    @body = client_response.body_io
   end
 end
