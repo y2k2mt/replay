a1202227e805f783d2a5d41abe48da76a1dceca5
y2k2mt <y2.k2mt@gmail.com>
2022-07-19 22:48:31 +0900

    Add response status

--- src/record.cr
+++ src/record.cr
@@ -1,14 +1,16 @@
 struct Record
-  getter headers, body
+  getter headers, body, response_status
 
   @headers : HTTP::Headers
   @body : String
+  @response_status : Int32
 
   def initialize(client_response)
     @headers = client_response.headers
     @body = client_response.body
+    @response_status = client_response.status_code
   end
 
-  def initialize(@headers : HTTP::Headers, @body : String)
+  def initialize(@headers : HTTP::Headers, @body : String,@response_status : Int32)
   end
 end
--- src/recorder.cr
+++ src/recorder.cr
@@ -22,8 +22,11 @@ class FileSystemRecorder
     if (!File.directory?(@reply_file_dir))
       Dir.mkdir_p(@reply_file_dir)
     end
+
+    record_headers_hash = record.headers.to_h
+    record_headers_hash["response_status"] = [record.response_status.to_s]
     File.open("#{@reply_file_dir}/#{index_hash}_headers", "w+")
-    File.write("#{@reply_file_dir}/#{index_hash}_headers", record.headers.to_h.to_json)
+    File.write("#{@reply_file_dir}/#{index_hash}_headers", record_headers_hash.to_json)
     File.open("#{@reply_file_dir}/#{index_hash}", "w+")
     File.write("#{@reply_file_dir}/#{index_hash}", record.body)
     {index, record}
@@ -37,12 +40,16 @@ class FileSystemRecorder
       Replay::Log.debug { "FileSystemRecorder: header_file path #{header_file}" }
       Replay::Log.debug { "FileSystemRecorder: body_file path #{body_file}" }
       response_headers = HTTP::Headers.new
-      Hash(String, Array(String)).from_json(JSON.parse(File.read(header_file)).to_json).map do |k, v|
+      #header_file_hash = Hash(String, Array(String)).from_json(JSON.parse(File.read(header_file)))
+      header_file_hash = Hash(String, Array(String)).from_json(File.read(header_file))
+      header_file_hash.map do |k, v|
+        if k != "response_status"
         response_headers[k] = v
+        end
       end
       response_body = File.read(body_file)
       Replay::Log.debug { "FileSystemRecorder: recorded response headers: #{response_headers}" }
-      Record.new(response_headers, response_body)
+      Record.new(response_headers, response_body, header_file_hash["response_status"].first.to_i32 )
     else
       Replay::Log.debug { "FileSystemRecorder: header_file or body_file not avairable." }
       nil
