75b4f67d541b134646346e0c7bded6667b6c03a3
y2k2mt <y2.k2mt@gmail.com>
2022-07-20 23:09:46 +0900

    Rename

--- spec/replay_spec.cr
+++ spec/replay_spec.cr
@@ -1,6 +1,6 @@
 require "./spec_helper"
 
-describe Parrot do
+describe Replay do
   # TODO: Write tests
 
   it "works" do
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,2 @@
 require "spec"
-require "../src/parrot"
+require "../src/replay"
