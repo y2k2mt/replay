74b3201901f8770a775dd1abb80d04120257b319
y2k2mt <y2.k2mt@gmail.com>
2022-07-27 22:51:36 +0900

    Add index params

--- src/replay/index.cr
+++ src/replay/index.cr
@@ -1,26 +1,30 @@
 struct Index
+  getter id, host_name, method, path, headers, params
+  @id : String
   @host_name : String
   @method : String
   @path : String
   @headers : Hash(String, Array(String))
-  @id : String
+  @params : Hash(String, String)
   @indexed_header_names : Array(String)
 
   def initialize(config, request)
+    @id = Random::Secure.hex
     @host_name = config.base_uri_host
     @path = request.path
     @method = request.method
     @headers = request.headers.to_h
-    @id = Random::Secure.hex
+    @params = request.query_params.to_h
     @indexed_header_names = [] of String
   end
 
   private def initialize(
+    @id,
     @host_name,
     @path,
     @method,
     @headers,
-    @id,
+    @params,
     @indexed_header_names = [] of String
   )
   end
@@ -28,14 +32,18 @@ struct Index
   def self.from(index_content : String)
     index = JSON.parse(index_content)
     new(
+      id = index["id"].to_s,
       host_name = index["host"].to_s,
       path = index["path"].to_s,
       method = index["method"].to_s,
-      headers = index["indexed"].as_h.reduce({} of String => Array(String)) { |acc, (k, v)|
+      headers = index["indexed"]["headers"].as_h.reduce({} of String => Array(String)) { |acc, (k, v)|
         acc[k] = v.as_a.map(&.to_s)
         acc
       },
-      id = index["id"].to_s
+      params = index["indexed"]["params"].as_h.reduce({} of String => String) { |acc, (k, v)|
+        acc[k] = v.as_s
+        acc
+      },
     )
   end
 
@@ -50,17 +58,25 @@ struct Index
   }
 
   def match?(index : Index) : Bool
-    index.meta_index == self.meta_index
+    index.meta_index == @meta_index &&
+      @headers.find { |k, v| !index.headers[k] || index.headers[k] != v } == nil &&
+      @params.find { |k, v| !index.params[k] || index.params[k] != v } == nil
   end
 
   def conditions
     {
-      "id"          => @id,
-      "host"        => @host_name,
-      "method"      => @method,
-      "path"        => @path,
-      "indexed"     => {} of String => Array(String),
-      "not_indexed" => @headers.to_h,
+      "id"      => @id,
+      "host"    => @host_name,
+      "method"  => @method,
+      "path"    => @path,
+      "indexed" => {
+        "headers" => {} of String => Array(String),
+        "params"  => {} of String => String,
+      },
+      "not_indexed" => {
+        "headers" => @headers,
+        "params"  => @params,
+      },
     }
   end
 end
