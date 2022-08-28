47e41680b96d541815ff1bca6660f3cd0a69399c
y2k2mt <y2.k2mt@gmail.com>
2022-08-28 14:23:10 +0900

    Split handler

--- src/replay2/server.cr
+++ src/replay2/server.cr
@@ -6,12 +6,22 @@ class Server
   def start : Void
     Replay::Log.info { "Listening on 0.0.0.0:#{@config.port}" }
     Replay::Log.info { "Running as #{@config.mode}ing mode for #{@config.base_uri}" }
+    handler = (case @config.mode
+    when Config::Mode::Record
+      ->(client : IO) { handle_record(client) }
+    when Config::Mode::Replay
+      ->(client : IO) { handle_replay(client) }
+    end)
     while client = @server.accept?
-      spawn handle_client(client)
+      spawn do
+        handler.try do |h|
+          h.call(client)
+        end
+      end
     end
   end
 
-  def handle_client(io)
+  def handle_record(io)
     case maybe_requests = @config.requests
     when Requests
       case maybe_record = Recorder.record(io, maybe_requests, @config.datasource)
@@ -28,6 +38,9 @@ class Server
       # TODO: response err
       Replay::Log.error { "Unsupported protocol: #{maybe_requests.protocol}" }
     end
+  end
+
+  def handle_replay(io)
   end
 
   def stop : Void
