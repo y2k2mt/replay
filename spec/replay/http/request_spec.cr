b68168eefa336f4babcefcd7c5e3b12510d408fc
y2k2mt <y2.k2mt@gmail.com>
2022-09-24 17:57:07 +0900

    Add specs

--- spec/replay/http/request_spec.cr
+++ spec/replay/http/request_spec.cr
@@ -20,4 +20,62 @@ describe HTTPRequest do
     actual_metadatas["indexed"]["body"].should eq("")
     actual_metadatas["not_indexed"]["body"].should eq("HELLO")
   end
+  it "can compare json proeprties" do
+    headers = HTTP::Headers{
+      "Content-Type" => "application/json",
+    }
+    json = %q{
+      {
+        "foo":"bar",
+        "baz":"qux",
+        "hoge": {
+          "fuga": 1,
+          "moge":"bar",
+          "baz" : [1,3,2]
+        }
+      }
+    }
+
+    another_json = %q{
+      {
+        "foo":"bar",
+        "hoge": {
+          "baz" : [1,3,2]
+        }
+      }
+    }
+    request = HTTP::Request.new("POST", "/hello", headers, json)
+    request1 = HTTPRequest.new(request, URI.parse "http://base.uri")
+    request2 = HTTPRequest.new(id: "foo", base_uri: URI.parse("http://base.uri"), path: "/hello", method: "POST", headers: headers.to_h, body: another_json, params: {} of String => String)
+    (request2 == request1).should be_true
+  end
+  it "can not compare json proeprties" do
+    headers = HTTP::Headers{
+      "Content-Type" => "application/json",
+    }
+    json = %q{
+      {
+        "foo":"bar",
+        "baz":"qux",
+        "hoge": {
+          "fuga": 1,
+          "moge":"bar",
+          "baz" : [1,3,2]
+        }
+      }
+    }
+    another_json = %q{
+      {
+        "foo":"bar",
+        "hoge": {
+          "fuga": 2,
+          "baz" : [1,3,2]
+        }
+      }
+    }
+    request = HTTP::Request.new("POST", "/hello", headers, json)
+    request1 = HTTPRequest.new(request, URI.parse "http://base.uri")
+    request2 = HTTPRequest.new(id: "foo", base_uri: URI.parse("http://base.uri"), path: "/hello", method: "POST", headers: headers.to_h, body: another_json, params: {} of String => String)
+    (request2 == request1).should be_false
+  end
 end
