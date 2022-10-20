8c7a6c41825903a46e02e7510b626a0f3878ad9b
y2k2mt <y2.k2mt@gmail.com>
2022-10-20 23:07:22 +0900

    Fix error handling

--- src/cli.cr
+++ src/cli.cr
@@ -5,46 +5,54 @@ mode = Config::Mode::Replay
 base_url : String? = nil
 query_options : Array(String) = [] of String
 
-parsed = OptionParser.parse do |parser|
-  parser.banner = "Replay: Record and Preplay!"
+begin
+  parsed = OptionParser.parse do |parser|
+    parser.banner = "Replay: Record and Preplay!"
 
-  parser.on "-v", "--version", "Show version" do
-    puts Replay::VERSION
-    exit
-  end
+    parser.on "-v", "--version", "Show version" do
+      puts Replay::VERSION
+      exit
+    end
 
-  parser.on "-h", "--help", "Show help" do
-    puts parser
-    exit
-  end
+    parser.on "-h", "--help", "Show help" do
+      puts parser
+      exit
+    end
 
-  parser.on "-p PORT", "--port PORT", "Server port" do |port|
-    port.to_i16?.try do |port_number|
-      server_port = port_number
+    parser.on "-p PORT", "--port PORT", "Server port" do |port|
+      port.to_i16?.try do |port_number|
+        server_port = port_number
+      end
+    end
+
+    parser.on "-r URL", "--record URL", "Run as recording mode" do |url|
+      mode = Config::Mode::Record
+      base_url = url
+    end
+
+    parser.on "-R URL", "--replay URL", "Run as replaying mode" do |url|
+      mode = Config::Mode::Replay
+      base_url = url
+    end
+
+    parser.on "-f URL", "--find URL", "Find resources" do |_|
+      ARGV[1..]?.try do |query_options|
+        pp query_options
+        base_url = query_options[0]?
+        mode = Config::Mode::Replay
+      end || (STDERR.puts parser; exit(1))
+    end
+
+    parser.invalid_option do |flag|
+      STDERR.puts "ERROR: #{flag} is not a valid option."
+      STDERR.puts parser
+      exit(1)
     end
   end
-
-  parser.on "-r URL", "--record URL", "Run as recording mode" do |url|
-    mode = Config::Mode::Record
-    base_url = url
-  end
-
-  parser.on "-R URL", "--replay URL", "Run as replaying mode" do |url|
-    mode = Config::Mode::Replay
-    base_url = url
-  end
-
-  parser.on "-f URL", "--find URL", "Find resources" do |_|
-    query_options = ARGV[1..]
-    base_url = query_options[0]
-    mode = Config::Mode::Replay
-  end
-
-  parser.invalid_option do |flag|
-    STDERR.puts "ERROR: #{flag} is not a valid option."
-    STDERR.puts parser
-    exit(1)
-  end
+rescue ex
+  puts ex
+  STDERR.puts parsed
+  exit(1)
 end
 
 if !base_url
